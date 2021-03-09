71acdb029aa9ed86e4fcbe4a059940c12e09d391
Rodrigo R. da Silva <rodrigoridasi@gmail.com>
2021-03-09 19:08:29 -0300

    List Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { Counter } from './components/Counter'
 import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 export default function App() {
     return (
-        <>
-            <RepositoryList />
-            <Counter />
-        </>
+        <RepositoryList />
     )
 }
